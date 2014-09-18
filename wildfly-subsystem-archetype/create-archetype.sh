225dadcc13619c0202cf1ab1773fa1fddb3de6ee
Rafael Benevides <rafabene@gmail.com>
2014-09-18 07:47:11 -0300

    Update groupdId to org.wilfly.archetype

--- wildfly-subsystem-archetype/create-archetype.sh
+++ wildfly-subsystem-archetype/create-archetype.sh
@@ -56,7 +56,7 @@ if [ ! -z $1 ] && [ "$1" = "generate" ]; then
     echo Generating project from archetype...
     cd target
     mvn archetype:generate -B -DarchetypeCatalog=local \
-        -DarchetypeArtifactId=wildlfly-subsystem -DarchetypeGroupId=org.wildfly.archetypes -DarchetypeVersion=8.0.0.Final \
+        -DarchetypeArtifactId=wildlfly-subsystem -DarchetypeGroupId=org.wildfly.archetype -DarchetypeVersion=8.0.0.Final \
         -DartifactId=example-subsystem -DgroupId=com.acme -Dpackage=com.acme.example -Dversion=1.0.0-SNAPSHOT -Dname="WildFly 8 subsystem project" \
         -Dmodule=org.test.subsystem
 fi
